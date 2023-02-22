b48beb60357f66c54d57ba733a375b189e78a885
Amdadul Haq <amdadulhaq781@gmail.com>
2023-02-22 23:16:45 +0600

    tests added

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Amdad121\CurrencyExchange\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
